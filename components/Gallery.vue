b950cbec7070616ba3d0d55b0dfba3e0054e8330
Zhanabaymalika <zhanabaymalika@gmail.com>
2024-01-26 20:07:26 +0600

    first commit

---
+++ components/Gallery.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h2>Gallery View</h2>
+      
+    </div>
+  </template>
+  
+  <script>
+
+  
+  export default {
+    name: 'GalleryComponent',
+    
+  };
+  </script>
+  
+  <style scoped>
+  
+  .content {
+    padding: 20px;
+  }
+  </style>
+  
